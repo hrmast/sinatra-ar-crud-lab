a3f0812b21982f959b601cec55ca9959522101b6
Heath Mast <hrmast@gmail.com>
2022-02-17 16:05:16 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,41 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles/new' do
+    erb :new
   end
+
+  post '/articles' do
+    @article = Article.create(title: params[:title], content: params[:content])
+
+    redirect "/articles/#{Article.last.id}"
+  end
+
+  get '/articles' do
+    @articles = Article.all
+
+    erb :index  
+  end
+  
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @edit = Article.find(params[:id])
+    binding.pry
+    erb :edit
+  end
+  
+  patch '/articles/:id' do 
+    @edit = Article.find(params[:id])
+    @edit.update(params[:article])
+  end
+
+  
+
+  #erb :show - displays an individual article
+  #erb :index - displays a list of all articles
 end
